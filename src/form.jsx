f46e90a24055c43849b47fedd5c3652b85fdf651
Jeff Dyer <jeffdyer@acm.org>
2022-11-02 15:53:58 -0700

    First crack dynamically imported form

---
+++ src/form.jsx
@@ -0,0 +1,12 @@
+//import { useState } from 'react';
+//import { useSelector } from 'react-redux'
+
+export function Form({ name }) {
+  //const hello = useSelector((state) => state.hello);
+  //console.log("L0 hello=" + hello);
+  return (
+    <div>
+      hello, { name }!!!
+    </div>
+  );
+}
